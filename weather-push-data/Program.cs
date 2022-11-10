4e7f8a8e754d812b3626575efa57c7cf40ebc9c5
acquiescent <54900535+acquiescent@users.noreply.github.com>
2022-11-10 11:43:06 +0800

    testing retry logic

--- weather-push-data/Program.cs
+++ weather-push-data/Program.cs
@@ -2,14 +2,40 @@ using Microsoft.EntityFrameworkCore;
 using WeatherApi.Data;
 
 // See if there's a way to incorporate connection_string.txt into appsettings.json?
+// TODO: Implement retry logic
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<WeatherContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("WeatherDb"))
-);
+
+// Try connecting
+const int retryIntervalSeconds = 6;
+const int retryAttempts = 10;
+bool connected = false;
+int retriesAttempted = 0;
+while (++retriesAttempted < retryAttempts)
+{
+    Console.WriteLine("Attempting connection {0}/{1} in {2} seconds...", retriesAttempted, retryAttempts, retryIntervalSeconds);
+    Thread.Sleep(retryIntervalSeconds * 1000);
+    try
+    {
+        builder.Services.AddDbContext<WeatherContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("WeatherDb"))
+        );
+        connected = true;
+    } 
+    catch (Exception e)
+    {
+        Console.WriteLine("Failed to connect.\n" + e);
+    }
+}
+if (!connected)
+{
+    Console.WriteLine("Failed to connect to DB. Aborting.");
+    return;
+}
+    
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
@@ -22,7 +48,5 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
