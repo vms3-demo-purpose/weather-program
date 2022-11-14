f8c3bf7c9f9d97b52376770e650ac090ce30c1f0
acquiescent <54900535+acquiescent@users.noreply.github.com>
2022-11-14 09:41:36 +0800

    debug

--- weather-push-data/Program.cs
+++ weather-push-data/Program.cs
@@ -1,53 +1,58 @@
 using Microsoft.EntityFrameworkCore;
 using WeatherApi.Data;
 
-// See if there's a way to incorporate connection_string.txt into appsettings.json?
-// TODO: Implement retry logic
+// 14 Nov: Fix program not reading appsettings.json's connection string
 
-var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-// Try connecting
-const int retryIntervalSeconds = 6;
-const int retryAttempts = 10;
-bool connected = false;
-int retriesAttempted = 0;
-while (++retriesAttempted < retryAttempts && !connected)
+try
 {
-    Console.WriteLine("Attempting connection {0}/{1} in {2} seconds...", retriesAttempted, retryAttempts, retryIntervalSeconds);
-    Thread.Sleep(retriesAttempted > 1 ? retryIntervalSeconds * 1000 : 0);
-    try
+    var builder = WebApplication.CreateBuilder(args);
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+
+    // Try connecting
+    const int retryIntervalSeconds = 6;
+    const int retryAttempts = 10;
+    bool connected = false;
+    int retriesAttempted = 0;
+    while (++retriesAttempted < retryAttempts && !connected)
     {
-        builder.Services.AddDbContext<WeatherContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("WeatherDb"))
-        );
-        connected = true;
-    } 
-    catch (Exception e)
-    {
-        Console.WriteLine("Failed to connect.\n" + e);
+        Console.WriteLine("Attempting connection {0}/{1} in {2} seconds...", retriesAttempted, retryAttempts, retryIntervalSeconds);
+        Thread.Sleep(retriesAttempted > 1 ? retryIntervalSeconds * 1000 : 0);
+        try
+        {
+            builder.Services.AddDbContext<WeatherContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("WeatherDb"))
+            );
+            connected = true;
+        } 
+        catch (Exception e)
+        {
+            Console.WriteLine("Failed to connect.\n" + e);
+        }
     }
-}
-if (!connected)
+    if (!connected)
+    {
+        Console.WriteLine("Failed to connect to DB. Aborting.");
+        return;
+    }
+        
+    var app = builder.Build();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var weatherContext = scope.ServiceProvider.GetRequiredService<WeatherContext>();
+        weatherContext.Database.EnsureCreated();
+        weatherContext.Seed();
+    }
+
+    // Configure the HTTP request pipeline.
+
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+} catch (Exception e)
 {
-    Console.WriteLine("Failed to connect to DB. Aborting.");
-    return;
+    Console.WriteLine(e);
 }
-    
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var weatherContext = scope.ServiceProvider.GetRequiredService<WeatherContext>();
-    weatherContext.Database.EnsureCreated();
-    weatherContext.Seed();
-}
-
-// Configure the HTTP request pipeline.
-
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
 Console.Read();
