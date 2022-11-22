358541e81e744c4ffcea3ac57b77233626302995
acquiescent <54900535+acquiescent@users.noreply.github.com>
2022-11-22 14:16:25 +0800

    Cleaned up comments

--- weather-push-data/Program.cs
+++ weather-push-data/Program.cs
@@ -1,27 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using WeatherApi.Data;
 
-// 14 Nov: Fix program not reading appsettings.json's connection string
-
+// Create builder
 var builder = WebApplication.CreateBuilder();
+
+// Add configuration files to builder
 builder.Configuration.AddJsonFile("appsettings.json");
 builder.Configuration.AddJsonFile("appsettings.Development.json");
     
+// Add services to builder    
 builder.Services.AddControllers();
 builder.Services.AddDbContext<WeatherContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("WeatherDb"))
 );
 
+// Build based on config files and services
 var app = builder.Build();
 
+// Populate database with Seed()
 using (var scope = app.Services.CreateScope())
 {
     var weatherContext = scope.ServiceProvider.GetRequiredService<WeatherContext>();
     weatherContext.Database.EnsureCreated();
     weatherContext.Seed();
 }
-
-// Configure the HTTP request pipeline.
 
 app.UseAuthorization();
 app.MapControllers();
