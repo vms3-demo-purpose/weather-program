3d4987bafa9573e34dbff335a328db6eaca25244
acquiescent <54900535+acquiescent@users.noreply.github.com>
2022-12-09 15:12:41 +0800

    simplified manual pw fill-in process

--- weather-push-data/Program.cs
+++ weather-push-data/Program.cs
@@ -9,10 +9,16 @@ var builder = WebApplication.CreateBuilder();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<WeatherContext>(options =>
     {
-        string source = System.IO.File.ReadAllText("/run/secrets/my-secret");
+        string source = File.ReadAllText("/run/secrets/my-secret");
         dynamic data = JObject.Parse(source);
-        string secretConnectionString = data.CONNECTION_STRING;
-        options.UseSqlServer(secretConnectionString);
+        string password = data.DB_PASSWORD;
+
+        string connectionString = 
+            "Server=weather-save-data;Database=Master;User Id=SA;Password=" +
+            password +
+            ";MultipleActiveResultSets=true;";
+        
+        options.UseSqlServer(connectionString);
     }
 );
 
